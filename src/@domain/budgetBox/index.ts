f9ff608ac0b4663f85b4a783218508f3318c851f
christiangsn <christianguimaraes1996@gmail.com>
2023-02-25 18:00:42 -0300

    feat: add index to export budget value-objects

--- src/@domain/budgetBox/index.ts
+++ src/@domain/budgetBox/index.ts
@@ -1,2 +1,5 @@
 export * from './valueObjects/budgetDescriptionValueObject'
 export * from './valueObjects/budgetPercentageValueObject'
+export * from './valueObjects/budgetReasons/budgetReasonDescriptionValueObject'
+export * from './valueObjects/budgetReasons/budgetReasonsIdValueObject'
+export * from './valueObjects/budgetIDValueObject'
