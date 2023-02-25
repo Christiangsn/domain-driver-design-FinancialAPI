30eb67d97739bec03998b4ef4360919066bd528a
christiangsn <christianguimaraes1996@gmail.com>
2023-02-25 18:03:17 -0300

    test: ensure create valid BudgetReasonId value object

---
+++ tests/@domain/budgetBox/valueObjects/budgetReasons/budgetReasonIdValueObject.spec.ts
@@ -0,0 +1,16 @@
+import { ReasonIdValueObject } from '@domain/budgetBox'
+import { UniqueEntityID } from '@domain/shared'
+
+describe('BudgetReasonIDValueObject', () => {
+  it('Should create a valid reasonID', () => {
+    const reasonId = ReasonIdValueObject.create()
+    expect(reasonId.isSuccess).toBe(true)
+    expect(reasonId.getResult().id.toValue).toBeDefined()
+  })
+
+  it('Should create a valid reasonId with value', () => {
+    const reasonId = ReasonIdValueObject.create(new UniqueEntityID('valid_id'))
+    expect(reasonId.isSuccess).toBe(true)
+    expect(reasonId.getResult().id.toValue).toBeDefined()
+  })
+})
