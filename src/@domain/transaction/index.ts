19324b91e9ac938f3e71ceba827043ec9fa8d43b
christiangsn <christianguimaraes1996@gmail.com>
2023-02-27 01:01:49 -0300

    chore: exports files transaction

---
+++ src/@domain/transaction/index.ts
@@ -0,0 +1,4 @@
+export * from './valuesObjects/transactionTypeValueObject'
+export * from './valuesObjects/statusTransactionValueObject'
+export * from './valuesObjects/transactionNoteValueObject'
+export * from './valuesObjects/attachmentValueObject'
