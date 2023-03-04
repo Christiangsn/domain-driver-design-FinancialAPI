456e2bff7334ab3fb9f1db2f5df93956189b3213
christiangsn <christianguimaraes1996@gmail.com>
2023-03-04 00:21:46 -0300

    feat: improved imports contracts repositories files

---
+++ src/@infra/shared/index.ts
@@ -0,0 +1,4 @@
+export * from './core/contracts/connectionContract'
+export * from './core/contracts/genericContractRepository'
+export * from './core/contracts/mappersContract'
+export * from './core/genericAbstractRepository'
