b0787e4df053d81b8bd02b80ce789eab1f1fe0be
christiangsn <christianguimaraes1996@gmail.com>
2023-03-04 00:23:08 -0300

    feat: [infra] - create a userSchema

---
+++ src/@infra/user/entities/userSchema.ts
@@ -0,0 +1,50 @@
+import { type OS } from '@domain/user'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { type Document } from 'mongoose'
+
+type IUserAgent = {
+  name: string
+  version: string
+  os: keyof typeof OS
+  type: string
+}
+
+type ITermsTypes = {
+  ip: string
+  acceptedAt: Date
+  userAgent: IUserAgent
+}
+
+@Schema({
+  autoCreate: true,
+  timestamps: true,
+  autoIndex: true
+})
+export class User {
+  @Prop({ immutable: true, required: true, type: String, index: true })
+  public readonly id!: string
+
+  @Prop({ required: true, index: true, type: String })
+  public email!: string
+
+  @Prop({ required: true, type: String })
+  public password!: string
+
+  @Prop({ type: [String], default: [] })
+  public budgetBoxIds!: string[]
+
+  @Prop({ type: Number, required: true, default: 0 })
+  public totalBalanceAvaliable!: number
+
+  @Prop({ type: Array<ITermsTypes>, required: true })
+  public terms!: ITermsTypes[]
+
+  @Prop({ type: Date, required: true, default: new Date() })
+  public createdAt!: Date
+
+  @Prop({ type: Date, required: true, default: new Date() })
+  public updatedAt!: Date
+}
+
+export type UserDocument = User & Document
+export const UserSchema = SchemaFactory.createForClass(User)
