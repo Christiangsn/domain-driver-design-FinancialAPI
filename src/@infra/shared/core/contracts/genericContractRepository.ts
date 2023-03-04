95cb2d586313a41eb3037061a45fbd82c8bdadf2
christiangsn <christianguimaraes1996@gmail.com>
2023-03-04 00:22:33 -0300

    feat: created a genericContractRepository

---
+++ src/@infra/shared/core/contracts/genericContractRepository.ts
@@ -0,0 +1,18 @@
+export type IFilterInterface = Record<string, string | number>
+
+/**
+   * `TargetPersistence` as Entity to persist on database and
+   * `DomainAggregate` as Aggregate Entity
+   * `ORM` as instance or connection or installed ORM
+   * @example UserPersistence from infra
+   * @example UserAggregate from domain
+   * @example ORM return method types `findOne` `finMany` ...
+   *
+   */
+export interface IGenericRepositoryContract< TargetPersistence, DomainAggregate, ORM > {
+  save: (entity: TargetPersistence) => Promise<void>
+  delete: (id: string) => Promise<void>
+  find: (filter: IFilterInterface) => Promise<DomainAggregate[] | null>
+  exist: (filter: IFilterInterface) => Promise<boolean>
+  orm: () => ORM
+}
