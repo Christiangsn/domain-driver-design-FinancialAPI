f832c1c84cb66ee4233b81fc6f0216d3ea51eb95
christiangsn <christianguimaraes1996@gmail.com>
2023-03-04 00:22:17 -0300

    feat: created a connectionContract

---
+++ src/@infra/shared/core/contracts/connectionContract.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-invalid-void-type */
+
+import { type IFilterInterface } from './genericContractRepository'
+
+/**
+ * `TargetEntity` as Entity to persist on database and
+ * `ORM` as instance or connection or installed ORM
+ * @example UserPersistence from infra
+ * @example TypeORM installed instance
+ *
+ * @method save:
+ * @method delete:
+ * @method find:
+ * @method orm:
+ */
+export interface IConnectionContract<TargetEntity, ORM> {
+  save: (entity: TargetEntity) => Promise<void | TargetEntity>
+  delete: (id: string) => Promise<void | TargetEntity>
+  find: (filter: IFilterInterface) => Promise<TargetEntity[] | null>
+  orm: () => ORM
+}
