1741284c387c631b862cec3c5bea6302f6cc8597
christiangsn <christianguimaraes1996@gmail.com>
2023-03-04 00:19:51 -0300

    feat: remove trash files

--- src/@infra/repositories/user/contractsPersistence/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './userPersistence'
-export * from './userRepositoryPersistence'
--- src/@infra/repositories/user/contractsPersistence/userRepositoryPersistence.ts
+++
@@ -1,5 +0,0 @@
-import { IGenericRepositoryContract } from "@infra/repositories/shared/core/contracts/genericRepository.contract";
-
-export interface IUserRepositoryPersistence<ITargetUserPersistence, ORM> extends IGenericRepositoryContract<ITargetUserPersistence> {
-    methods: () => ORM
-}
