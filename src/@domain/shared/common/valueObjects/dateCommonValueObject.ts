1470350bab5410e0d2f3e8cbe76ea89f6e161f90
christiangsn <christianguimaraes1996@gmail.com>
2023-03-04 17:06:02 -0300

    fix: ensure a date value new Date

--- src/@domain/shared/common/valueObjects/dateCommonValueObject.ts
+++ src/@domain/shared/common/valueObjects/dateCommonValueObject.ts
@@ -8,19 +8,20 @@ export class DateCommonValueObject extends ValueObject<IDateCommonProps> {
     super(props)
   }
 
-  public get value (): Date {
-    const locateDate = new Date(this.props.value)
-    const dateStr =
-    ('00' + (locateDate.getMonth() + 1)).slice(-2) + '-' +
-    ('00' + locateDate.getDate()).slice(-2) + '-' +
-    locateDate.getFullYear() + ' ' +
-    ('00' + locateDate.getHours()).slice(-2) + ':' +
-    ('00' + locateDate.getMinutes()).slice(-2) + ':' +
-    ('00' + locateDate.getSeconds()).slice(-2)
+  // public get value (): Date {
+  //   const locateDate = new Date(this.props.value)
+  //   const dateStr =
+  //   ('00' + (locateDate.getMonth() + 1)).slice(-2) + '-' +
+  //   ('00' + locateDate.getDate()).slice(-2) + '-' +
+  //   locateDate.getFullYear() + ' ' +
+  //   ('00' + locateDate.getHours()).slice(-2) + ':' +
+  //   ('00' + locateDate.getMinutes()).slice(-2) + ':' +
+  //   ('00' + locateDate.getSeconds()).slice(-2)
 
-    return dateStr as unknown as Date
-  }
+  //   return dateStr as unknown as Date
+  // }
 
+  public get value (): Date { return this.props.value }
   public static create (date: Date): Result<DateCommonValueObject> {
     // Validation typeof Date
     if (!(date instanceof Date && !isNaN(date.valueOf()))) return Result.fail<DateCommonValueObject>('Invalid Date')
