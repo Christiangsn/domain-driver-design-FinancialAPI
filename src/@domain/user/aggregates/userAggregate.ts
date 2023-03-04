76a2249ae1d7a6033306ab194131751a772818d5
christiangsn <christianguimaraes1996@gmail.com>
2023-03-04 00:21:09 -0300

    refactor: updated an comments class UserAggregate

--- src/@domain/user/aggregates/userAggregate.ts
+++ src/@domain/user/aggregates/userAggregate.ts
@@ -4,7 +4,7 @@ import { AggregateRoot } from '@domain/shared/core/aggregateRoot'
 import { type EmailValueObject } from '../valueObjects/emailValueObject'
 import { type PasswordValueObject } from '../valueObjects/passwordValueObject'
 import { type TermsValueObject } from '../valueObjects/termsValueObject'
-import { Result } from '@domain/shared/core'
+import { type BaseDomainEntity, Result } from '@domain/shared/core'
 
 type IUserAggregateProps = {
   email: EmailValueObject
@@ -12,8 +12,15 @@ type IUserAggregateProps = {
   budgetBoxIds?: BudgetIdValueObject[]
   totalBalanceAvaliable: number
   terms: TermsValueObject[]
-}
+} & BaseDomainEntity
 
+/**
+ * @var email 'EmailValueObject'
+ * @var password 'PasswordValueObject'
+ * @var budgetBoxIds 'BudgetIdValueObject[]'
+ * @var totalBalanceAvaliable 'number'
+ * @var budgetBoxIds 'TermValueObject[]'
+ */
 export class UserAggregate extends AggregateRoot<IUserAggregateProps> {
   private constructor (props: IUserAggregateProps, id?: UniqueEntityID) {
     super(props, id)
@@ -24,6 +31,7 @@ export class UserAggregate extends AggregateRoot<IUserAggregateProps> {
   public get budgetBoxIds (): BudgetIdValueObject[] { return this.props.budgetBoxIds ?? [] }
   public get terms (): TermsValueObject[] { return this.props.terms }
   public get totalBalanceAvaliable (): number { return this.props.totalBalanceAvaliable }
+  public get deletedAt (): Date { return this.props.deletedAt ?? new Date() }
 
   public static create (props: IUserAggregateProps, id?: UniqueEntityID): Result<UserAggregate> {
     return Result.ok<UserAggregate>(new UserAggregate(props, id))
