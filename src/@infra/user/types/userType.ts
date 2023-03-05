3e5fe80df197d9cefac241adc9b512c40b34845e
christiangsn <christianguimaraes1996@gmail.com>
2023-03-05 03:58:16 -0300

    feat: [infra] - create a userType #12

---
+++ src/@infra/user/types/userType.ts
@@ -0,0 +1,14 @@
+import { TermType } from './termType'
+import { Field, ID, ObjectType } from '@nestjs/graphql'
+
+@ObjectType()
+export class UserType {
+  @Field(() => ID)
+  public id!: string
+
+  @Field(() => [TermType], { nullable: true })
+  public terms!: TermType[]
+
+  @Field(() => String)
+  public email!: string
+}
