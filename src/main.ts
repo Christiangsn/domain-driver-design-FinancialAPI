ffc0cab44f4132553adfb0dc229a01733522d2d4
christiangsn <christianguimaraes1996@gmail.com>
2023-03-05 03:52:28 -0300

    config: add a nestjs

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+/* eslint-disable  */
+import 'module-alias/register'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap () {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(4000)
+}
+bootstrap()
