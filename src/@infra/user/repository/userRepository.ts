cff296b386639b03513e2d45f9ac53d952839197
christiangsn <christianguimaraes1996@gmail.com>
2023-03-07 19:48:49 -0300

    feat: implementation default methods in UserRepository #9

--- src/@infra/user/repository/userRepository.ts
+++ src/@infra/user/repository/userRepository.ts
@@ -1,18 +1,24 @@
-import { type UserDocument } from './../entities/userSchema'
+import { User, type UserDocument } from './../entities/userSchema'
 import { type IUserRepositoryContract } from '@domain/contracts/repositories/userRepository.contract'
 import { type UserAggregate } from '@domain/user'
 import { type IFilterInterface } from '@infra/shared'
-import { type UserMapper } from '../mappers/userMapper'
+import { UserMapper } from './userMapper'
 import { type Model } from 'mongoose'
+import { Inject } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
 
 export class UserRepository implements IUserRepositoryContract {
   public constructor (
+    @Inject(UserMapper)
     private readonly mapper: UserMapper,
+    @InjectModel(User.name)
     private readonly conn: Model<UserDocument>
   ) {}
 
   public async save (entity: UserAggregate): Promise<void> {
-    throw new Error('Method not implemented.')
+    const schema = this.mapper.toPersistence(entity)
+    const user = new this.conn(schema)
+    await user.save()
   }
 
   public async delete (id: string): Promise<void> {
@@ -24,6 +30,6 @@ export class UserRepository implements IUserRepositoryContract {
   }
 
   public async exist (filter: IFilterInterface): Promise<boolean> {
-    throw new Error('Method not implemented.')
+    return await this.conn.exists(filter)
   }
 }
