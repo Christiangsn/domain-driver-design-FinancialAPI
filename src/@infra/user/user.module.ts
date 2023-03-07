a1df6d9f5ef46ff745b3a6e3e077db79c2a85893
christiangsn <christianguimaraes1996@gmail.com>
2023-03-07 19:47:58 -0300

    feat: create a userModule #10

--- src/@infra/user/user.module.ts
+++ src/@infra/user/user.module.ts
@@ -1,9 +1,26 @@
+import { User, UserSchema } from './entities/userSchema'
+import { MongooseModule } from '@nestjs/mongoose'
+import { UserMapper } from './repository/userMapper'
+import { SignupUseCases } from '@application/useCases/user'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { UserResolver } from './resolver/user.resolver'
+import { UserService } from './user.service'
+import { UserRepository } from './repository/userRepository'
 
 @Module({
-  imports: [],
-  providers: [UserResolver],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+  ],
+  providers: [
+    { provide: 'IUserService', useClass: UserService },
+    { provide: 'ISignUpUseCase', useClass: SignupUseCases },
+    { provide: 'IUserRepository', useClass: UserRepository },
+    UserMapper,
+    UserResolver,
+    UserService
+  ],
   exports: []
 })
 export class UserModule {}
