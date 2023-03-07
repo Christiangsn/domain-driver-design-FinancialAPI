4c46fafdae290d895b293dd3b4229dec97c7e603
christiangsn <christianguimaraes1996@gmail.com>
2023-03-07 19:45:06 -0300

    fix: dependency inject and return domain Result for SignUpUseCases #6

--- src/@application/useCases/user/signup/signUpUseCases.ts
+++ src/@application/useCases/user/signup/signUpUseCases.ts
@@ -2,43 +2,31 @@
 
 import { DateCommonValueObject, EmailValueObject, IpValueObject, PasswordValueObject, TermsValueObject, UserAggregate } from '@domain/user'
 import { type IUserRepositoryContract } from '@domain/contracts/repositories/userRepository.contract'
-import { type IHttpResponse } from '@application/contracts/app.contracts'
-import { useCases } from '@application/useCases/default'
 import { Result } from '@domain/shared/core'
-
 import { type ISignUpDTO } from './signupDTO'
+import { useCases } from '@application/useCases/useCases'
+import { Inject } from '@nestjs/common'
 
-import { BadRequest, Success } from '@application/helpers'
+export interface IUseCases<DTO, Return> {
+  run: (props: DTO) => Promise<Return>
+}
 
-/**
- * @event
- *   Validar dados da request
- *  @event
- *   Criar instancia do domínio do usuário
- * @event
- *   Verificar se existem dados incorretos
- * @event
- *   Verificar se já existe um usuario com email informado
- * @event
- *   Salvar usuário
- * @event
- *   Retornar status de sucesso
- */
 export class SignupUseCases extends useCases<ISignUpDTO> {
   public constructor (
+    @Inject('IUserRepository')
     private readonly userRepository: IUserRepositoryContract
-  ) {
-    super('Iterrnal Server Error on Signup Use Case')
-  }
+  ) { 
+    super('SignUp')
+   }
 
-  public async run (props: ISignUpDTO): Promise<IHttpResponse> {
+  public async run (props: ISignUpDTO): Promise<Result<void>> {
     // Validar os DTO e se ocorreu um erro retornar
     const emailOrError = EmailValueObject.create(props.email)
     const passwordOrError = PasswordValueObject.create(props.password)
     const accepetAtOrError = DateCommonValueObject.create(new Date())
     const ipOrError = IpValueObject.create(props.ip)
     const hasErrorOnValueObjects = Result.combine([emailOrError, passwordOrError, accepetAtOrError, ipOrError])
-    if (hasErrorOnValueObjects.isFailure) return BadRequest(hasErrorOnValueObjects.error)
+    if (hasErrorOnValueObjects.isFailure) return Result.fail<void>(hasErrorOnValueObjects.error)
 
     // Validar Termos e se ocorreu um erro retornar
     const termsOrError = TermsValueObject.create({
@@ -51,7 +39,7 @@ export class SignupUseCases extends useCases<ISignUpDTO> {
         version: props.userAgent.version
       }
     })
-    if (termsOrError.isFailure) return BadRequest(termsOrError.error.toString())
+    if (termsOrError.isFailure) return Result.fail<void>(termsOrError.error.toString())
 
     const password = passwordOrError.getResult()
     void await password.encryptPassword()
@@ -61,16 +49,16 @@ export class SignupUseCases extends useCases<ISignUpDTO> {
       password,
       terms: [termsOrError.getResult()]
     })
-    if (userOrError.isFailure) return BadRequest(userOrError.error.toString())
+    if (userOrError.isFailure) return Result.fail<void>(userOrError.error.toString())
 
     // Verificar se o usuário já existe
     const alreadyExistsForEmail = await this.userRepository.exist({ email: userOrError.getResult().email.value })
-    if (alreadyExistsForEmail) return BadRequest('User already exists')
+    if (alreadyExistsForEmail) return Result.fail<void>('User already exist for provided email')
 
     // Salvar usuario
     const user = userOrError.getResult()
     void await this.userRepository.save(user)
 
-    return Success(null)
+    return Result.ok<void>()
   }
 }
