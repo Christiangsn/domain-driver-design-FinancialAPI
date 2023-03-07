00986d7df634a353cda949bde7bb3ead9bbf34e8
christiangsn <christianguimaraes1996@gmail.com>
2023-03-07 19:50:18 -0300

    feat: create a userType #13

--- src/@infra/user/types/userType.ts
+++ src/@infra/user/types/userType.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { TermType } from './termType'
 import { Field, ID, ObjectType } from '@nestjs/graphql'
 
@@ -6,7 +7,7 @@ export class UserType {
   @Field(() => ID)
   public id!: string
 
-  @Field(() => [TermType], { nullable: true })
+  @Field(() => [TermType]!, { nullable: true })
   public terms!: TermType[]
 
   @Field(() => String)
