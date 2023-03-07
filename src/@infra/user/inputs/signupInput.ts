082d187cd3b78fd81efc434020d502956e259e51
christiangsn <christianguimaraes1996@gmail.com>
2023-03-07 19:45:31 -0300

    feat: create a signupInput #13

---
+++ src/@infra/user/inputs/signupInput.ts
@@ -0,0 +1,16 @@
+import { Field, InputType } from '@nestjs/graphql'
+
+@InputType()
+export class SignUpInput {
+  @Field(() => String)
+  public email!: string
+
+  @Field(() => String)
+  public password!: string
+
+  @Field(() => String!)
+  public ip!: string
+
+  @Field(() => Boolean!)
+  public acceptedTerms!: boolean
+}
