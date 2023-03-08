a1e22d246bb73c0d326712d8497c248b42f4a7e2
christiangsn <christianguimaraes1996@gmail.com>
2023-03-08 19:13:55 -0300

    fix: [aaplication] - updated a contract interface useCases #6 Essa interface estava gerando erro nos testes, pois a importação estava falhando

--- src/@application/contracts/app.contracts.ts
+++ src/@application/contracts/app.contracts.ts
@@ -1,12 +1,3 @@
-export interface IHttpResponse<Data = any> {
-  statusCode: number
-  data: Data
-}
-/**
- *  Validation request data
- *  Create a new instances domain
- *  Verify incorrect parameters
- */
-export interface IUseCasesAPP<DTO> {
-  run: (props: DTO) => Promise<IHttpResponse>
+export interface IUseCases<DTO, Return> {
+  run: (props: DTO) => Promise<Return>
 }
